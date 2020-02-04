b039074573620642a4d0d6b659e04c4081234666
Alex Lubinets <123qwe789>
2020-02-04 15:48:41 +0200

    UserProfile added

---
+++ TestProj/TestProj/Controllers/UserProfileController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TestProj.Models;
+
+namespace TestProj.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //GET: /api/UserProfile
+    public class UserProfileController : ControllerBase
+    {
+        private readonly UserManager<AppUser> userManager;
+
+        public UserProfileController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<Object> GetUserProfile()
+        {
+            string userId = User.Claims.First(c => c.Type == "Id").Value;
+            var user = await userManager.FindByIdAsync(userId);
+            return new
+            {
+                user.UserName,
+                user.FullName,
+                user.Email,
+            };
+        }
+    }
+}
