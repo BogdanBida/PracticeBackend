fdba7a1fd2c11ca993e694eff9913e092b9a5ec7
Alex Lubinets <123qwe789>
2020-02-06 18:04:05 +0200

    Fix HomeController
    (no validation)

--- TestProj/TestProj/Controllers/HomeController.cs
+++ TestProj/TestProj/Controllers/HomeController.cs
@@ -27,5 +27,47 @@ namespace TestProj.Controllers
         {
             return repo.GetItemList();
         }
+
+        [HttpGet]
+        [Route("ProductDetails/{id}")]
+        //GET: api/Home/ProductDetails/id
+        public Product GetProductDetails(int id)
+        {
+
+            var result = repo.GetItem(id);
+            return result;
+        }
+
+        [HttpPost]
+        [Route("CreateProduct")]
+        //GET: api/Home/CreateProduct
+        public Product CreateProduct(Product model)
+        {
+            repo.Create(model);
+            return (model);
+        }
+
+        [HttpPost]
+        [Route("DeleteProduct/{id}")]
+        //GET: api/Home/DeleteProdu ct/id
+        public Product DeleteProduct(int id)
+        {
+            return repo.Delete(id);
+        }
+
+        [HttpPost]
+        [Route("UpdateProduct")]
+        //GET: api/Home/UpdateProduct
+        public Product UpdateProduct(Product modelChanges)
+        {
+            try
+            {
+                return repo.Update(modelChanges);
+            }
+            catch(Exception ex)
+            {
+                throw (ex);
+            }
+        }
     }
 }
